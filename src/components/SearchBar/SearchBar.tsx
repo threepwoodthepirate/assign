6d14f7416067df95b8991682e986f31437fac1be
jimmieespling <jimmie.espling@gmail.com>
2022-09-17 08:37:08 +0200

    Added logo and searchbar component

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,21 @@
+import { SearchIcon } from "@/icons/SearchIcon";
+import { SearchProps } from "./props";
+
+const SearchBar: React.FC<SearchProps> = ({ disabled = false }) => {
+  return (
+    <div className="flex items-center justify-between p-2 pb-2 mx-auto mt-4 mb-4 space-x-2 transition duration-300 ease-in border rounded-lg border-slate-700 align-items bg-slate-700">
+      <SearchIcon></SearchIcon>
+      <label className="hidden" htmlFor="search">
+        Search
+      </label>
+      <input
+        autoComplete="off"
+        name="search"
+        placeholder="Search by name.."
+        className="w-full px-2 py-2 leading-6 transition duration-300 ease-in rounded-md text-md focus:outline-none bg-slate-700 text-gray-50 placeholder-slate-200"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/features/Home/components/Home.tsx
+++ src/features/Home/components/Home.tsx
@@ -3,15 +3,26 @@ import type { NextPage } from "next";
 import { UsersProps } from "@/features/Users/props";
 import PageLayout from "@/components/Layout/PageLayout";
 import { UserCards } from "@/features/Users/components";
+import { Logo } from "@/components/Logo";
+import SearchBar from "@/components/SearchBar/SearchBar";
 
 export const Home: NextPage<UsersProps> = ({ userList }) => {
   console.log(userList.length);
   return (
     <PageLayout>
       <div className="h-full">
-        <div className="flex flex-col md:flex-row justify-between items-center">
+        <Logo></Logo>
+        <div className="flex flex-col items-center justify-between mb-2 md:flex-row">
           <h1 className="my-4 sm:my-0">Welcome to the fellowship of 1337</h1>
+          <button
+            title="Show users in a plain list and grid list"
+            // onClick={}
+            className="h-10 px-4 text-sm font-normal text-white transition duration-300 ease-in bg-blue-600 rounded-md shadow-xl  font-poppins hover:text-blue-100 hover:bg-blue-600"
+          >
+            Toggle listview
+          </button>
         </div>
+        <SearchBar disabled={false}></SearchBar>
         <UserCards userFilteredList={userList}></UserCards>
       </div>
     </PageLayout>
